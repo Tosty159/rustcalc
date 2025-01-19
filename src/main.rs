e79ed2a2debdd827a176c2af57723fb793a7f855
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-01-19 16:43:30 +0100

    Add interpret() to turn AST into an f64.

--- src/main.rs
+++ src/main.rs
@@ -175,7 +175,7 @@ impl<'a> Parser<'a> {
     fn parse_factor(&mut self) -> ASTNode {
         match self.current_token {
             Token::Number(num) => ASTNode::Number(num),
-            Token::Operator(op) if op == '-' => {
+            Token::Operator(op) if op == '+' || op == '-' => {
                 let operand = Box::new(self.parse_factor());
                 ASTNode::UnaryOperator { operand, op }
             },
@@ -185,6 +185,32 @@ impl<'a> Parser<'a> {
                 node
             },
             _ => panic!("Unexpected token: {:?}", self.current_token),
+        }
+    }
+}
+
+fn interpret(ast: ASTNode) -> f64 {
+    match ast {
+        ASTNode::Number(num) => num,
+        ASTNode::UnaryOperator { operand, op } => {
+            let opr = interpret(*operand);
+            match op {
+                '+' => opr,
+                '-' => -opr,
+                _ => panic!("Invalid unary operator: {op}"),
+            }
+        },
+        ASTNode::BinaryOperator { lhs, op, rhs } => {
+            let left = interpret(*lhs);
+            let right = interpret(*rhs);
+
+            match op {
+                '+' => left + right,
+                '-' => left - right,
+                '*' => left * right,
+                '/' => left / right,
+                _ => panic!("Invalid binary oeprator: {op}"),
+            }
         }
     }
 }
