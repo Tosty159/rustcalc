4d1bd4fdadcc3a1c4594a48df9c82eb795c6dc7e
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-01-19 13:54:15 +0100

    Add function get_input() to read input from user.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{stdin, stdout, Write};
+
+fn get_input() -> String {
+    let mut input = String::new();
+
+    print!("Calc> ");
+    stdout().flush().unwrap();
+
+    stdin().read_line(&mut input).expect("That's not a valid input...");
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+
+    input
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("RustCalc Alpha 1.0");
+    println!("Press Ctrl+c to terminate.");
+    println!("\n");
+
+    loop {
+        let input = get_input();
+    }
 }
