f63ef7abf4f5005d39f7cde0eab7ab1a1266a779
Jaden El-Gharabi <waelelgharabi245@gmail.com>
2025-01-21 17:37:14 +0100

    Change get_input() to handle correct syntax on its own.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use core::panic;
 use std::io::{stdin, stdout, Write};
 use std::collections::HashSet;
 use once_cell::sync::Lazy;
+use termion::{raw::IntoRawMode, input::TermRead};
 
 static ALLOWED: Lazy<HashSet<char>> = Lazy::new(|| {
     [' ', '\t','0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '(', ')']
@@ -18,20 +19,35 @@ fn check_syntax(s: String) -> String {
 }
 
 fn get_input() -> String {
+    let stdin = std::io::stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
     let mut input = String::new();
 
-    print!("Calc> ");
-    stdout().flush().unwrap();
+    write!(stdout, "Calc> ").unwrap();
+    stdout.flush().unwrap();
 
-    stdin().read_line(&mut input).expect("That's not a valid input...");
-    if let Some('\n') = input.chars().next_back() {
-        input.pop();
+    for key in stdin.keys() {
+        match key.unwrap() {
+            termion::event::Key::Char(c) if ALLOWED.contains(&c) => {
+                input.push(c);
+                write!(stdout, "{c}").unwrap();
+                stdout.flush().unwrap();
+            }
+            termion::event::Key::Backspace => {
+                if !input.is_empty() {
+                    input.pop();
+                    write!(stdout, "\u{8} \u{8}").unwrap();
+                    stdout.flush().unwrap();
+                }
+            },
+            termion::event::Key::Char('\n') => break,
+            _ => {} // Ignore invalid keys
+        }
     }
-    if let Some('\r') = input.chars().next_back() {
-        input.pop();
-    }
+    write!(stdout, "\n").unwrap();
+    stdout.flush().unwrap();
 
-    check_syntax(input)
+    input
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
